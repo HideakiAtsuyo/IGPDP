ed2a3b9a0d17b36ba1be4b916dd70678d5664658
Dany <53636771+Dany-LF@users.noreply.github.com>
2020-01-01 23:38:49 +0100

    Mise à jour
    Ajout de la possiblité de choix

--- igpdp.js
+++ igpdp.js
@@ -1,24 +1,67 @@
+/////DÉPENDANCES/////
+/////////////////////
 const request = require ('request');
 const snekfetch = require ('snekfetch');
 const dl = require ('download-file');
 const chalk = require('chalk');
+/////////////////////
 
+/////DES IF INUTILES CAR JE SUPPOSE QU'ON PEUT LE SIMPLIFIER/////
+/////////////////////////////////////////////////////////////////
 if(process.argv.length <= 2) {
-	console.log(chalk.red.underline("Utilisation: node igpdp.js <pseudo>\nExemple:\nnode igpdp.js ahbahjesuisfou"))
-	process.exit(-1)
+	utilisation()
 }
-var p = process.argv[2];
-var r6 = snekfetch['get']('https://www.instagram.com/' + p + '?__a=1');
-r6.send().then(r => {
-	if(`${r.body.graphql.user.is_private}` == true){
-		var pv = "Oui";
-		} else {
-		var pv = "Non";
-		}
-		console.log(chalk.red.bold(`Nom: ${r.body.graphql.user.username} || ID: ${r.body.graphql.user.id}\nSurnom: ${r.body.graphql.user.full_name||"Aucun surnom"}\nDescription: \"${r.body.graphql.user.biography||"Aucune biographie"}\"\nAbonnés: ${r.body.graphql.user.edge_followed_by.count}\nAbonnements: ${r.body.graphql.user.edge_follow.count}\nPhotos&Vidéos: ${r.body.graphql.user.edge_owner_to_timeline_media.count}\nPrivé: ${pv}\n\nLien Photo de profile: \n${r.body.graphql.user.profile_pic_url_hd}`))
+if(process.argv.length <= 3) {
+	utilisation()	
+}
+/////////////////////////////////////////////////////////////////
 
+/////VARIABLES/////
+///////////////////
+var opt = process.argv[2];
+var p = process.argv[3];
+///////////////////
+
+/////CODE/////
+//////////////
+if(opt == "-P" || opt == "--picture"){
+	cl()
+	var r6 = snekfetch['get']('https://www.instagram.com/' + p + '?__a=1');
+	r6.send().then(r => {
 		var pdp = `${r.body.graphql.user.profile_pic_url_hd}`;
-		var accpdp = {filename: `${process.argv[2]}_${r.body.graphql.user.id}.jpg`}
-		dl(pdp, accpdp)
-		console.log(chalk.blue.italic("\n\nPDP téléchargé!"))
-})
+		var accpdp = {filename: `${r.body.graphql.user.username}_${r.body.graphql.user.id}.jpg`}
+		dl(pdp, accpdp);
+		console.log(chalk.blue.italic(`La pdp de ${r.body.graphql.user.username} a été téléchargée avec succès!`));
+		setTimeout(stop, 2000, 'Fin');
+	})
+} else if(opt == "-I" || opt == "--infos"){
+	cl();
+	var r6 = snekfetch['get']('https://www.instagram.com/' + p + '?__a=1');
+	r6.send().then(r => {
+		if(`${r.body.graphql.user.is_private}` == true){
+			var pv = "Oui";
+		} else {
+			var pv = "Non";
+		}
+		console.log(chalk.red.bold(`Nom: ${r.body.graphql.user.username} || ID: ${r.body.graphql.user.id}\nSurnom: ${r.body.graphql.user.full_name||"Aucun surnom"}\nDescription: \"${r.body.graphql.user.biography||"Aucune biographie"}\"\nAbonnés: ${r.body.graphql.user.edge_followed_by.count}\nAbonnements: ${r.body.graphql.user.edge_follow.count}\nPhotos&Vidéos: ${r.body.graphql.user.edge_owner_to_timeline_media.count}\nPrivé: ${pv}\n\nLien Photo de profile: \n${r.body.graphql.user.profile_pic_url_hd}`));
+		setTimeout(stop, 2000, 'Fin');
+	});
+} else {
+	cl();
+	utilisation();
+}
+//////////////
+
+/////FONCTIONS/////
+///////////////////
+function utilisation(){
+	console.log(chalk.red.underline("Utilisation: node igpdp.js <option> <pseudo>\nExemple:\nnode igpdp.js -P ahbahjesuisfou\n\n=====Options:=====\n-P ou --picture pour télécharger une photo de profile\n-I ou --infos pour obtenir les informations d'un profile\n==================\n\nSi il y a un bug merci d'ouvrir une issue ici: https://github.com/Dany-LF/IGPDP/issues"));
+	process.exit(-1);
+}
+function stop(){
+	process.exit(-1);
+}
+function cl(){
+	console.clear();
+}
+///////////////////
