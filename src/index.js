c252f445b528d8970d30abdbd2990de0873a5982
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-10-25 23:35:01 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -17,16 +17,16 @@ async function main() {
 
         downloadFile(pdp, { filename }, function () {
             console.log(chalk.blue.italic(`The ${user.username}'s avatar has been downloaded at ${filename}!`));
-            setTimeout(exit, 2000, 'Fin');
+            setTimeout(exit, 2000, 'End');
         });
     } else if (['-I', '--infos'].includes(opt)) {
-        const is_private = user.is_private ? 'Oui' : 'Non',
-            is_verified = user.is_verified ? 'Oui' : 'Non',
-            is_joined_recently = user.is_joined_recently ? 'Oui' : 'Non',
-            is_business_account = user.is_business_account ? 'Oui' : 'Non';
+        const is_private = user.is_private ? 'Yes' : 'No',
+            is_verified = user.is_verified ? 'Yes' : 'No',
+            is_joined_recently = user.is_joined_recently ? 'Yes' : 'No',
+            is_business_account = user.is_business_account ? 'Yes' : 'No';
 
         console.log(chalk.red.bold(`Username: ${user.username} || ID: ${user.id}\nFull name: ${user.full_name || 'No full name found'}\nDescription: \"${user.biography || 'No biography found'}\"\nFollowers: ${user.edge_followed_by.count}\nFollowing: ${user.edge_follow.count}\nPosts: ${user.edge_owner_to_timeline_media.count}\nIs private: ${is_private} || Is verified: ${is_verified}\nRecently joined: ${is_joined_recently} || Is a business account: ${is_business_account}`));
-        setTimeout(exit, 2000, 'Fin');
+        setTimeout(exit, 2000, 'End');
     } else {
         usage()
     }
