1b060f2c824d1f1414098908ef5994dda3ee5f86
Hideaki <53636771+HideakiAtsuyo@users.noreply.github.com>
2021-11-04 13:17:52 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -19,15 +19,10 @@ async function main() {
     if (['-P', '--picture'].includes(opt)) {
         const avatarURL = user.profile_pic_url_hd,
             output = `${user.username}_${user.id}.jpg`;
-
-
+        
         try {
             await downloadProfilePicture(avatarURL, output);
-            prettyPrinter(
-                `The ${user.username}'s avatar has been downloaded at ${output}!`,
-                'blue',
-                'italic'
-            );
+            prettyPrinter(`The ${user.username}'s avatar has been downloaded at ${output}!`, 'blue', 'italic');
             setTimeout(exit, 2000, 'End');
         } catch (e) {
             throw e;
@@ -38,19 +33,7 @@ async function main() {
             is_verified = user.is_verified ? 'Yes' : 'No',
             is_joined_recently = user.is_joined_recently ? 'Yes' : 'No',
             is_business_account = user.is_business_account ? 'Yes' : 'No';
-        prettyPrinter(
-            `Username: ${user.username} || ID: ${user.id}\nFull name: ${
-                user.full_name || 'No full name found'
-            }\nDescription: \"${
-                user.biography || 'No biography found'
-            }\"\nFollowers: ${user.edge_followed_by.count}\nFollowing: ${
-                user.edge_follow.count
-            }\nPosts: ${
-                user.edge_owner_to_timeline_media.count
-            }\nIs private: ${is_private} || Is verified: ${is_verified}\nRecently joined: ${is_joined_recently} || Is a business account: ${is_business_account}`,
-            'red',
-            'bold'
-        );
+        prettyPrinter(`Username: ${user.username} || ID: ${user.id}\nFull name: ${user.full_name || 'No full name found'}\nDescription: \"${user.biography || 'No biography found'}\"\nFollowers: ${user.edge_followed_by.count}\nFollowing: ${user.edge_follow.count}\nPosts: ${user.edge_owner_to_timeline_media.count}\nIs private: ${is_private} || Is verified: ${is_verified}\nRecently joined: ${is_joined_recently} || Is a business account: ${is_business_account}`, 'red', 'bold');
         setTimeout(exit, 2000, 'End');
     } else {
         usage();
